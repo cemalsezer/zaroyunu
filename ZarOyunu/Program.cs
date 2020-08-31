d49fcbe4d8edfcf235dab603a2955ceb224ebde3
cemalsezer <69629934+cemalsezer@users.noreply.github.com>
2020-08-31 19:01:07 +0300

    First Commit

---
+++ ZarOyunu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ZarOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TavlaZari tavlaZari1 = new TavlaZari(8);
+            Console.WriteLine("Zar: "+tavlaZari1.ZarAt());
+            
+            //
+        }
+    }
+}
